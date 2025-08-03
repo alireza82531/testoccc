1487b6bc221ce937de0b3aca5816323fe73bba5e
alireza82531 <Alireza82531@gmail.com>
2025-08-03 15:46:59 +0330

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching TR Party Subscribers", "Watching TR Party Server"];
+const statusMessages = ["Watching Ace Subscribers", "Watching Ace Team Server"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
